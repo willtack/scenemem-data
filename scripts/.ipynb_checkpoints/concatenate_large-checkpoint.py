570a747caf5bc88ee260a9d8e1a8c6af5e732b43
Will Tackett <will.s.tackett@gmail.com>
2020-09-16 13:46:09 -0400

    09-16 13:46

--- scripts/.ipynb_checkpoints/concatenate_large-checkpoint.py
+++ scripts/.ipynb_checkpoints/concatenate_large-checkpoint.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 
-datadir = "csvs2"
+datadir = "csvs/csvs2"
 # columns = ["subject","task","whole brain left %","broca's area left %","inferior frontal gyrus left %","middle frontal gyrus left %","superior frontal gyrus left %","frontal lobe left %","inferior temporal gyrus left %","middle temporal gyrus left %","superior temporal gyrus left %","planum temporale left %","angular gyrus left %","whole brain right %","broca's area right %","inferior frontal gyrus right %","middle frontal gyrus right %","superior frontal gyrus right %","frontal lobe right %","inferior temporal gyrus right %","middle temporal gyrus right %","superior temporal gyrus right %","planum temporale right %","angular gyrus right %","whole brain LI","broca's area LI","inferior frontal gyrus LI","middle frontal gyrus LI","superior frontal gyrus LI","frontal lobe LI","inferior temporal gyrus LI","middle temporal gyrus LI","superior temporal gyrus LI","planum temporale LI","angular gyrus LI","whole brain left voxels","broca's area left voxels","inferior frontal gyrus left voxels","middle frontal gyrus left voxels","superior frontal gyrus left voxels","frontal lobe left voxels","inferior temporal gyrus left voxels","middle temporal gyrus left voxels","superior temporal gyrus left voxels","planum temporale left voxels","angular gyrus left voxels","whole brain right voxels","broca's area right voxels","inferior frontal gyrus right voxels","middle frontal gyrus right voxels","superior frontal gyrus right voxels","frontal lobe right voxels","inferior temporal gyrus right voxels","middle temporal gyrus right voxels","superior temporal gyrus right voxels","planum temporale right voxels","angular gyrus right voxels","whole brain sum","broca's area sum","inferior frontal gyrus sum","middle frontal gyrus sum","superior frontal gyrus sum","frontal lobe sum","inferior temporal gyrus sum","middle temporal gyrus sum","superior temporal gyrus sum","planum temporale sum","angular gyrus sum","FramewiseDisplacement","tSNR"]
 
 columns = ["subject", "task", "mTL left %", "Hippocampus left %", "Amygdala left %", "Parahippocampal left %", "Entorhinal cortex left %", "Fusiform gyrus left %",
@@ -27,4 +27,4 @@ for file in os.listdir(datadir):
 
 master_df = pd.concat(df_list, ignore_index=True)
 master_df.set_index('subject', drop=True, inplace=True) # change index to subject IDs
-master_df.to_csv(os.path.join(datadir, '..', 'data_09-11.csv'))
+master_df.to_csv(os.path.join(datadir, '..', 'data_09-15.csv'))
--- scripts/download_csvs.py
+++ scripts/download_csvs.py
@@ -9,7 +9,13 @@ analyses = fw.get_analyses("projects", project.id, "all")
 scenemem_analyses = [a for a in analyses if 'scenemem2' in a.label]
 
 for analysis in scenemem_analyses:
-    file = [f for f in analysis.files if "scenemem" in f.name]
+    subject = fw.get(analysis.parents['subject'])
+    print(subject.label)
+    try:
+        file = [f for f in analysis.files if "scenemem" in f.name]
+    except TypeError as e:
+        print(e)
+        continue
     if len(file) < 1:
         sub = fw.get(analysis.parents['subject'])
         print("{}: scenemem csv not found.".format(sub.label))
